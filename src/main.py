2c246386d4c36e3fef47a4d208acd4e35b8e5071
Luana <ltdc01@hotmail.com>
2021-12-01 22:27:32 -0300

    Added restrictions

--- src/main.py
+++ src/main.py
@@ -9,24 +9,35 @@ modelo.setParam('TimeLimit', 3*60)
 #Lendo numero de agentes e numero de tarefas do arquivo
 aux = input()
 num_agentes, num_tarefas = aux.split()
+#domínio das variaveis
 num_agentes = int(num_agentes)
 num_tarefas = int(num_tarefas)
 
-#Preparando leitura dos pesos
+#Preparando leitura dos inputs
 
 c = {}
+a = {}
+cap = {}
 
-#Transformando em matriz
+#Lendo a matriz de satisfação
 for i in range(num_agentes):
     aux = input()
     aux = aux.split()
     for j in range(num_tarefas):
         c[i,j] = int(aux[j])
 
+#Lendo a matriz de recursos (a)
+for i in range(num_agentes):
+    aux = input()
+    aux = aux.split()
+    for j in range(num_tarefas):
+        a[i,j] = int(aux[j])
 
-#TODO Adicionar a leitura dos recursos consumidos por cada agente
-
-#TODO Adicionar a leitura da capacidade de cada agente
+#Lendo linha com capacidades de cada agente (cap)
+aux = input()
+aux = aux.split()
+for i in range(num_agentes):
+    cap[i] = int(aux[i])
 
 
 #Adicionando a variavel binaria X
@@ -35,20 +46,18 @@ for i in range(num_agentes):
    for j in range(num_tarefas):
      xvar[i,j] = modelo.addVar(vtype=gurobi.GRB.BINARY, name='x'+'{'+str(i)+','+str(j)+'}')
 
-
 # Construindo a funcao objetivo
 modelo.setObjective(gurobi.quicksum(gurobi.quicksum(xvar[i,j]*c[i,j] for j in range(num_tarefas) ) for i in range(num_agentes)), GRB.MAXIMIZE)
 
-# Add constraint: x + 2 y + 3 z <= 4
+# Adicionando restricao: cada tarefa eh feita por somente um agente
 for i in range(num_agentes):
     modelo.addConstr(gurobi.quicksum(xvar[i,j] for j in range(num_tarefas)) == 1)
 
 
-#TODO Adicionar a restricao de recurso de cada agente SUM{j=1}( aij*xij ≤ capi)
+# Adicionando restricao: cada agente realiza tarefas de acordo com seus recursos
+for i in range(num_agentes):
+    modelo.addConstr(gurobi.quicksum(xvar[i,j]*a[i,j] for j in range(num_tarefas)) <= cap[i])
 
-
-#Possibilidade de escrever o modelo
-#modelo.write("modelo.lp")
 
 #Otimizando o modelo
 modelo.optimize()
